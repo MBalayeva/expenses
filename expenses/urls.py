fe13a2218d81242ac3dfc9ab8ad67a62a1ec5687
Mehriban Balayeva <mehribanbalayeva@MacBook-Air-Mehriban.local>
2021-09-22 09:53:14 +0400

    pafination added to expenses

--- expenses/urls.py
+++ expenses/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="expenses"), 
-    path("add-expenses/", views.add_expenses, name="add-expenses")
+    path("add-expenses/", views.add_expenses, name="add-expenses"),
+    path("edit-expense/<int:id>", views.edit_expense, name="edit-expense"),
+    path("delete-expense/<int:pk>/", views.ExpenseDeleteView.as_view(), name="delete-expense"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import Expense, Category
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+from django.core.paginator import Paginator
 
 # Create your views here.
 @login_required(login_url='authentication/login')
 def index(request):
-    return render(request, 'expenses/index.html')
+    expenses = Expense.objects.filter(owner=request.user)
+
+    paginator = Paginator(expenses, 2)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj
+    }
+    return render(request, 'expenses/index.html', context)
 
 
 def add_expenses(request):
@@ -41,3 +54,44 @@ def add_expenses(request):
         return redirect('expenses')
 
 
+def edit_expense(request, id):
+    expense = Expense.objects.get(pk=id)
+    categories = Category.objects.all
+    context = {
+        'expense': expense,
+        'categories': categories
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/edit-expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+        description = request.POST['description']
+        category = request.POST['category']
+        expense_date = request.POST['expense_date']
+
+
+        if not amount:
+            messages.error(request, 'Fill in the amount field')
+            return render(request, 'expenses/edit-expenses.html', context)
+
+        if not description:
+            messages.error(request, 'Fill in the description field')
+            return render(request, 'expenses/edit-expenses.html', context)
+        expense.owner = request.user
+        expense.amount = amount
+        expense.description = description
+        expense.category = category
+        if not expense_date:
+            expense.save()
+        else: expense.date = expense_date
+
+        expense.save()
+         
+        messages.success(request, "Expense updated successfully")
+        return redirect('expenses')
+
+
+class ExpenseDeleteView(DeleteView):
+    model = Expense
+    success_url = reverse_lazy('expenses')
